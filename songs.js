ae8e80fc6345a443b313ebe4796e616ef2752d58
rohangoenka6 <rohangoenka6@users.noreply.github.com>
2026-09-01 08:04:26 +0530

    Fix song mapping: use uniquely-named audio files (cache-bust)

--- songs.js
+++ songs.js
@@ -2,24 +2,24 @@
 //  The Love Rewind · songs
 //
 //  This file is the single source of truth for the playlist.
-//  audio: drop a matching file into the /songs folder
-//    (e.g. songs/01.mp3) and it will be picked up automatically.
+//  audio: each song points to a uniquely-named file in /songs
+//    (unique names also bust any cached copies on hosting/CDN).
 //  cover: fetched automatically from the iTunes Store by
 //    artist + title. To pin a specific image, set `cover`.
 // ============================================================
 
 var SONGS = [
-  { title: "Sing to Me", artist: "Walter Martin" },
-  { title: "Can't Help Falling in Love", artist: "Elvis Presley" },
-  { title: "Can't Help Falling In Love", artist: "Elvis Presley" },
-  { title: "Yellow", artist: "Coldplay" },
-  { title: "(Everything I Do) I Do It for You", artist: "Bryan Adams" },
-  { title: "Just the Way You Are", artist: "Bruno Mars" },
-  { title: "Leire Leire", artist: "Jayanta Nath" },
-  { title: "Night Changes", artist: "One Direction" },
-  { title: "Nirole Khani", artist: "Zubeen Garg" },
-  { title: "Thinking Out Loud", artist: "Ed Sheeran" },
-  { title: "Until I Found You", artist: "Stephen Sanchez" },
-  { title: "Wonderful Tonight", artist: "Eric Clapton" },
-  { title: "Yengejei Nangse", artist: "Zubeen Garg" },
+  { title: "Sing to Me", artist: "Walter Martin", file: "songs/sing-to-me.mp3" },
+  { title: "Can't Help Falling in Love", artist: "Elvis Presley", file: "songs/cant-help-falling-in-love.mp3" },
+  { title: "Can't Help Falling In Love", artist: "Elvis Presley", file: "songs/cant-help-falling-in-love-2.mp3" },
+  { title: "Yellow", artist: "Coldplay", file: "songs/yellow.mp3" },
+  { title: "(Everything I Do) I Do It for You", artist: "Bryan Adams", file: "songs/everything-i-do.mp3" },
+  { title: "Just the Way You Are", artist: "Bruno Mars", file: "songs/just-the-way-you-are.mp3" },
+  { title: "Leire Leire", artist: "Jayanta Nath", file: "songs/leire-leire.mp3" },
+  { title: "Night Changes", artist: "One Direction", file: "songs/night-changes.mp3" },
+  { title: "Nirole Khani", artist: "Zubeen Garg", file: "songs/nirole-khani.mp3" },
+  { title: "Thinking Out Loud", artist: "Ed Sheeran", file: "songs/thinking-out-loud.mp3" },
+  { title: "Until I Found You", artist: "Stephen Sanchez", file: "songs/until-i-found-you.mp3" },
+  { title: "Wonderful Tonight", artist: "Eric Clapton", file: "songs/wonderful-tonight.mp3" },
+  { title: "Yengejei Nangse", artist: "Zubeen Garg", file: "songs/yengejei-nangse.mp3" },
 ];
